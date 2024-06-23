e22ed39645665246381010313cdf6a06fb7f118c
Orinugoori <bigbangok@naver.com>
2024-06-24 02:48:35 +0900

    랜덤한 이미지 구현

--- app/src/main/java/com/example/orinugooriworldapp/HomeActivity.kt
+++ app/src/main/java/com/example/orinugooriworldapp/HomeActivity.kt
@@ -2,12 +2,15 @@ package com.example.orinugooriworldapp
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Random
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,11 +23,19 @@ class HomeActivity : AppCompatActivity() {
             insets
         }
 
+
+        val random = Random()
+        val randomIndex = random.nextInt(5)
+
+        changeView(randomIndex)
+
+
+
         val userIDData = intent.getStringExtra("UserID")
 
         Log.d("HomeActivity", "Received User ID: $userIDData")
 
-        var tvDisplayID = findViewById<TextView>(R.id.tv_id_display)
+        val tvDisplayID = findViewById<TextView>(R.id.tv_id_display)
 
         tvDisplayID.text = userIDData
 
@@ -34,6 +45,55 @@ class HomeActivity : AppCompatActivity() {
             finish()
         }
 
+
+
+
+    }
+
+    private fun changeView(index : Int){
+        val randomImage1 = findViewById<ImageView>(R.id.iv_home_character1)
+        val randomImage2 = findViewById<ImageView>(R.id.iv_home_character2)
+        val randomImage3 = findViewById<ImageView>(R.id.iv_home_character3)
+        val randomImage4 = findViewById<ImageView>(R.id.iv_home_character4)
+        val randomImage5 = findViewById<ImageView>(R.id.iv_home_character5)
+
+        when (index){
+            0 -> {
+                randomImage1.setTransitionVisibility(View.VISIBLE)
+                randomImage2.setTransitionVisibility(View.INVISIBLE)
+                randomImage3.setTransitionVisibility(View.INVISIBLE)
+                randomImage4.setTransitionVisibility(View.INVISIBLE)
+                randomImage5.setTransitionVisibility(View.INVISIBLE)
+            }
+            1 -> {
+                randomImage1.setTransitionVisibility(View.INVISIBLE)
+                randomImage2.setTransitionVisibility(View.VISIBLE)
+                randomImage3.setTransitionVisibility(View.INVISIBLE)
+                randomImage4.setTransitionVisibility(View.INVISIBLE)
+                randomImage5.setTransitionVisibility(View.INVISIBLE)
+            }
+            2 ->{
+                randomImage1.setTransitionVisibility(View.INVISIBLE)
+                randomImage2.setTransitionVisibility(View.INVISIBLE)
+                randomImage3.setTransitionVisibility(View.VISIBLE)
+                randomImage4.setTransitionVisibility(View.INVISIBLE)
+                randomImage5.setTransitionVisibility(View.INVISIBLE)
+            }
+            3 -> {
+                randomImage1.setTransitionVisibility(View.INVISIBLE)
+                randomImage2.setTransitionVisibility(View.INVISIBLE)
+                randomImage3.setTransitionVisibility(View.INVISIBLE)
+                randomImage4.setTransitionVisibility(View.VISIBLE)
+                randomImage5.setTransitionVisibility(View.INVISIBLE)
+            }
+            4 -> {
+                randomImage1.setTransitionVisibility(View.INVISIBLE)
+                randomImage2.setTransitionVisibility(View.INVISIBLE)
+                randomImage3.setTransitionVisibility(View.INVISIBLE)
+                randomImage4.setTransitionVisibility(View.INVISIBLE)
+                randomImage5.setTransitionVisibility(View.VISIBLE)
+            }
+        }
 
     }
 }
